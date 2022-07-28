dba46e9b4038e2282a48d9aadc51f9d71b3cbcae
Sabrina Kim <sabrinakim@fb.com>
2022-07-28 14:51:14 -0700

    only compares place name when matching

--- app/src/main/java/com/example/intandem/LoginActivity.java
+++ app/src/main/java/com/example/intandem/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.intandem.models.Friendship;
@@ -49,6 +50,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         callbackManager = CallbackManager.Factory.create();
 
